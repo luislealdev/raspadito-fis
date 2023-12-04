b0beca303172f930c6864c58b3dc49a015cd47cf
Luis Leal <luisrrleal@gmail.com>
2023-12-04 15:59:05 -0600

    Change designs and game working

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 var input = document.getElementById("Main-Input");
-
-//Botón para iniciar el juego (message.html)
-//var playButton = document.getElementById('Play-Button');
-
-// playButton.addEventListener("click", ()=>{
-//   console.log('HAY QUE JUGAR');
-//   generateRandomGifts();
-// });
+var button = document.getElementById("Main-Input-Button");
 
 // Login Button
 let winner = true;
@@ -17,30 +10,9 @@ function evaluateInputState() {
     button.style.display = "none";
   } else {
     button.style.display = "flex";
+    
   }
 }
-
-// function generateRandomGifts() {
-//   let giftsArray = [1, 1, 1, 1];
-//   let randPosition = Math.floor(Math.random() * 4);
-
-//   giftsArray[randPosition] = 0;
-
-//   for (let i = 0; i < 4; i++) {
-//     var giftBox = document.getElementById("Checkbox" + i);
-
-//     giftBox.setAttribute("data-valor", giftsArray[i]);
-
-//     giftBox.addEventListener("click", function () {
-//       let giftValue = parseInt(this.getAttribute("data-valor"));
-
-//       if (giftValue === 0) {
-//         winner = false;
-//         console.log("PERDISTE D:");
-//       }
-//     });
-//   }
-// }
 
 window.addEventListener("DOMContentLoaded", function () {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -99,58 +71,3 @@ window.addEventListener("DOMContentLoaded", function () {
   // Llamar a la función para iniciar el juego
   generateRandomGifts();
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-//   const giftImages = document.querySelectorAll(".Gift-Image");
-
-//   let selectedCount = 0;
-
-//   checkboxes.forEach((checkbox) => {
-//     checkbox.addEventListener("change", function () {
-//       if (this.checked) {
-//         selectedCount++;
-//         if (selectedCount === 3) {
-//           checkboxes.forEach((cb) => {
-//             if (!cb.checked) {
-//               cb.disabled = true;
-//             }
-//           });
-//         }
-//       } else {
-//         selectedCount--;
-//         checkboxes.forEach((cb) => {
-//           cb.disabled = false;
-//         });
-//       }
-//     });
-//   });
-
-//   function generateRandomGifts() {
-//     let giftsArray = [1, 1, 1, 1];
-//     let randPosition = Math.floor(Math.random() * 4);
-
-//     giftsArray[randPosition] = 0;
-
-//     for (let i = 0; i < 4; i++) {
-//       var giftBox = document.getElementById("Checkbox" + (i + 1));
-
-//       giftBox.setAttribute("data-valor", giftsArray[i]);
-
-//       giftBox.addEventListener("click", function () {
-//         let giftValue = parseInt(this.getAttribute("data-valor"));
-
-//         if (giftValue === 0) {
-//           giftImages[i].src = "./assets/christmas-tree_616662.png"; // Mostrar el árbol de Navidad
-//           giftImages[i].alt = "Árbol de Navidad";
-//         } else {
-//           giftImages[i].src = "./assets/christmas-cat.png"; // Mostrar el gatito
-//           giftImages[i].alt = "Gatito";
-//         }
-//       });
-//     }
-//   }
-
-//   // Llamar a la función para iniciar el juego
-//   generateRandomGifts();
-// });
